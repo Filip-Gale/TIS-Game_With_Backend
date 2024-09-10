d530e0317e4381cf5205c461d306d2052db1dcc0
fgale <filipgale6@gmail.com>
2024-09-10 11:47:29 +0200

    Quacky Setup

---
+++ product-manager/src/main/java/hr/tis/academy/App.java
@@ -0,0 +1,21 @@
+package hr.tis.academy;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+
+    }
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return args -> {
+
+        };
+    }
+}
+
